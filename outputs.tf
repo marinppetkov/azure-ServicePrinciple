53deb7c5199a227a5db9dd3ef8b7267db948d320
Marin Petkov <marin.petkov@hashicorp.com>
2024-03-12 17:09:26 +0200

    updating putputs

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,26 @@
-output "client_secret_id" {
-  value = azuread_application_password.client_secrets.key_id
-}
+# output "ARM_CLIENT_SECRET" {
+#   value = azuread_application_password.client_secrets.key_id
+# }
 
-output "client_value" {
+output "ARM_CLIENT_SECRET" {
   value =  azuread_application_password.client_secrets.value
   sensitive = true
 }
 
-output "subscription_id" {
+output "ARM_SUBSCRIPTION_ID" {
   description = "Azure subscription ID"
   value       = data.azurerm_subscription.current.subscription_id
 }
 
-output "tenant_id" {
+output "ARM_TENANT_ID" {
   description = "Azure tenant ID"
   value       = data.azurerm_subscription.current.tenant_id
 }
+
+output "ARM_CLIENT_ID" {
+  description = "ARM_CLIENT_ID"
+  value = azuread_application.tfc_app_access.client_id
+
+}
+
+# terraform output ARM_CLIENT_SECRET
